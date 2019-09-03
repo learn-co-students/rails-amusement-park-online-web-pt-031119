de6bcc8450224a67bc7c509db13a655a94b7177a
David Stewart <davidstewart@gmail.com>
2019-09-02 21:11:02 -0600

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
     has_secure_password
+    def mood
+        if self.nausea != nil && self.happiness != nil
+            if self.nausea > self.happiness
+                "sad"
+            elsif self.nausea < self.happiness
+                "happy"
+            end
+        end
+    end
 end
