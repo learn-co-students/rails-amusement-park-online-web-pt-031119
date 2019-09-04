6281ee87cb8d6083caccbb54c1c837b30e962747
arpethel <arpethel@gmail.com>
2019-09-04 14:48:31 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
   end
 end
--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -1,4 +1,8 @@
 class Attraction < ApplicationRecord
     has_many :rides
     has_many :users, through: :rides
+
+    # def count_users
+    #     user_count = Users.all.count
+    # end
 end
