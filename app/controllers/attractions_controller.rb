48e3b6f7a0e95b58aa31344a7f71d145210f938d
dsstewa <davidstewart@gmail.com>
2019-09-07 14:03:09 -0600

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -5,15 +5,26 @@ class AttractionsController < ApplicationController
     end
     
     def create
-   
-    
-    redirect_to user_path current_user
+     @attraction = Attraction.new(attraction_params)
+    @attraction.save
+
+    redirect_to attraction_path @attraction
     end
     
     def index
         @attractions = Attraction.all
     end
-    
+    def edit
+       @attraction = Attraction.find_by_id(params[:id])
+    end
+
+    def update
+        @attraction = Attraction.find_by(id: params[:id])
+        @attraction.update(attraction_params)
+        if @attraction.save
+            redirect_to attraction_path(@attraction)
+        end 
+    end
     
     def show
         @attraction = Attraction.find_by_id(params[:id])
