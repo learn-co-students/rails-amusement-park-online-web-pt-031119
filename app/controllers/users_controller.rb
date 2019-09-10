e262c3c4f050073868b611ba1dcf4c6ef312d725
homies886 <Niko876@hotmail.com>
2019-09-10 11:45:46 -0400

    Done

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+
+    def index
+        @users = User.all
+    end
+  
+    def show
+        if session[:user_id]
+            @user = User.find_by(id: params[:id])
+            @message = params[:message]
+        else
+            redirect_to '/'
+        end
+    end
+  
+    def new
+        @user = User.new
+    end
+  
+    def create
+      @user = User.create(user_params)
+      if @user
+          session[:user_id] = @user.id
+          redirect_to user_path(@user)
+      else
+          render :new
+      end
+    end
+  
+    private
+  
+    def user_params
+      params.require(:user).permit(:name, :password, :nausea, :happiness, :tickets, :height, :admin)
+    end
+  
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root 'users#new'
+  resources :users
+  resources :attractions, only: [:index, :show, :new, :create, :edit, :update]
+  resources :rides, only: [:new, :create]
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
 end
